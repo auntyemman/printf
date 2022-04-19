e94a5bfeb54b7c4128f31ee853119c2320a16dc5
Adedayo Adepoju <1mole.coco@gmail.com>
2022-04-19 13:50:18 +0100

    main test file

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(void)
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe6375;
+    addr = (void *)0x7ffe63f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -38,5 +38,9 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+    _printf("%b\n", 98);
+    printf("%b\n", 98);
+    _printf("%S\n", "Best\nSchool");
+    printf("%S\n", "Best\nSchool");
     return (0);
 }
